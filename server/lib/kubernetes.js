254437c7aa6c33429b192ce9c1dc5a66b775a90d
Santiago Rodriguez <sanrodari@gmail.com>
2018-02-20 20:35:19 -0500

    Build the Docker image

--- server/lib/kubernetes.js
+++ server/lib/kubernetes.js
@@ -2,10 +2,14 @@ const os = require('os');
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
+const crypto = require('crypto');
+const { exec } = require('child_process');
 
 const mkdtempPromise = util.promisify(fs.mkdtemp);
 const readdirPromise = util.promisify(fs.readdir);
 const copyFilePromise = util.promisify(fs.copyFile);
+const execPromise = util.promisify(exec);
+const randomBytesPromise = util.promisify(crypto.randomBytes);
 
 const PROJECT_TEMPLATE_PATH = path.resolve('../project-template');
 
@@ -26,10 +30,22 @@ async function buildProject(botFilePath) {
   return buildDirPath;
 }
 
+async function buildDockerImage(buildDirPath) {
+  const userId = '123'; // TODO use the userId to namespace the image
+  const randomName = (await randomBytesPromise(5)).toString('hex');
+  const imageName = `${userId}/${randomName}`;
+  const buildCommand = `docker build -t ${imageName} .`;
+  const { stdout } = await execPromise(buildCommand, { cwd: buildDirPath });
+  console.log('buildDockerImage stdout\n', stdout);
+
+  return imageName;
+}
+
 // Creates a Docker image using a bot definition file and a project template
 async function createDockerImage(botFilePath) {
   const buildDirPath = await buildProject(botFilePath);
-  // TODO
+  const imageName = await buildDockerImage(buildDirPath);
+  // TODO persist imageName for later usage
 }
 
 module.exports = {
