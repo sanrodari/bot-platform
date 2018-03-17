30ffc55a25717f26e55e01f3c9f8ab3ca29b38c7
Gustavo A. Lopez <gualopezb@gmail.com>
2018-03-17 12:19:26 -0500

    Handle error for get and delete bot endpoints

--- server/routes/bots.js
+++ server/routes/bots.js
@@ -22,12 +22,28 @@ router.patch('/:id', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const result = await botsRepo.findByUserAndId(req.user.id, req.params.id);
+
+  if (!result) {
+    res.status(404).send({ errors: { message: 'Bot not found' } });
+    return;
+  }
+
   res.json(result);
 });
 
 router.delete('/:id', async (req, res) => {
-  const result = await botsRepo.deleteByUserAndId(req.user.id, req.params.id);
-  res.json(result);
+  const { n, ok } = await botsRepo.deleteByUserAndId(req.user.id, req.params.id);
+
+  if (n !== 1) {
+    res.status(404).send({ errors: { message: 'Bot not found' } });
+    return;
+  }
+
+  const success = ok === 1;
+
+  res.json({
+    success,
+  });
 });
 
 module.exports = router;
